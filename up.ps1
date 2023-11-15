79a901eb1e75376718cf3451bbf565d6b4de5851
Milton Tom <83508961+miltontom@users.noreply.github.com>
2023-11-15 18:26:36 +0530

    Update up.ps1

--- up.ps1
+++ up.ps1
@@ -1,29 +1,17 @@
-function Set-MaxLimit($levelCount, $limit, $argCount) {
-    if ($argCount -eq 0) {
-        return 1
-    }
+$limit = ($args.Count -eq 0)? 1: $args[0];
 
-    if ($limit -ge $levelCount) {
-        return $levelCount - 1
-    }
+$path = Get-Location
+$levelCount = ($path -split "\\").Count
 
-    return $limit
-}
+$isInteger = ($limit -is [Int32])
+$isInRange = ($limit -lt $levelCount)
 
-if ($args.Count -gt 0 -and $args[0] -lt 0 ) {
-    Write-Host "Usage: up <limit>"
-    Write-Host "`nOptions:"
-    Write-Host "`tlimit - Positive integer"
+if (!$isInteger -or !$isInRange) {
+    Write-Host "Usage: up <valid level>"
     Exit
 }
 
-$limit = $args[0]
-$argCount = $args.Count
-$path = Get-Location
-$levelCount = ($path -split "\\").Count
-$maxLimit = Set-MaxLimit $levelCount $limit $argCount
-
-for ($i = 1; $i -le $maxLimit; $i++) {
-    # Go back one level in the hierarchy
+for ($i = 1; $i -le $limit; $i++) {
+    # Go back one level in the hierarchy (cd ..)
     Set-Location ..
 }
