ebc2bd465146962b08c3015a8e8c2d12506ae36c
Milton Tom <83508961+miltontom@users.noreply.github.com>
2023-11-17 08:47:34 +0530

    Update up.ps1

--- up.ps1
+++ up.ps1
@@ -1,16 +1,16 @@
-$limit = ($args.Count -eq 0)? 1: $args[0];
+$level = ($args.Count -eq 0)? 1: $args[0];
 
 $path = Get-Location
 $levelCount = ($path -split "\\").Count
 
-$isInteger = ($limit -is [Int32])
-$isInRange = ($limit -lt $levelCount)
+$isInteger = ($level -is [Int32])
+$isInRange = ($level -lt $levelCount)
 
 if (!$isInteger -or !$isInRange) {
     Write-Host "Usage: up <valid level>"
     Exit
 }
 
-for ($i = 1; $i -le $limit; $i++) {
+for ($i = 1; $i -le $level; $i++) {
     Set-Location ..
 }
